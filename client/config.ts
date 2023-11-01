276a80750f67bd8e9f1cda06d21ccc9c94fcb584
Mike Petrusha <petrushamike@gmail.com>
2023-11-01 11:16:35 +0300

    fix: delete broken nfts

--- client/config.ts
+++ client/config.ts
@@ -1,6 +1,5 @@
-export const nftmarketaddress = "0x2e30378FaAE42b61435a8DAA9D354C0B3269FA0c";
-export const erc721address = "0x15ff4d4239758E33E08EA0C3c7603350d4656E23";
-export const erc1155address = "0x32Ef4381432359e01784ccC3E8F90D4C50cDCE54";
-export const INFURALINK =
-  "https://sepolia.infura.io/v3/9eac74003e914d29abec44e635b26fb4";
+export const nftmarketaddress = '0xf7A96e78434412B8BE1AfA8Ac063C21a3B0Bcba2';
+export const erc721address = '0x573A947B945DF743F32633A05F23c1495841abdE';
+export const erc1155address = '0x560306Ffb2dD2104Bf70B3fbd0ABa266c0dD6bC5';
+export const INFURALINK = 'https://sepolia.infura.io/v3/9eac74003e914d29abec44e635b26fb4';
 export const NFTSTORAGETOKEN = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEyQ0NCZWQ5Zjc5NEREODU3NkNGOEVmOWE4OTk4QTM2NUIyMjgyYjUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5MDM4MDY1MzY0MSwibmFtZSI6Im5mdC1tYXJrZXRwbGFjZSJ9.rOKCZcZc7w4CWYac7_wQNigIuihiWhwxF-NVY7bnUYE`;
